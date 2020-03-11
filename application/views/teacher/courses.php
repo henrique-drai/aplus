786ff207a91fb702e91816f3947dcd9daf60c9bb
Inês Sousa <inessousa.sousa@gmail.com>
2020-03-11 14:51:42 +0000

    página dos projetos feita - front end

--- application/views/teacher/courses.php
+++ application/views/teacher/courses.php
@@ -1,4 +1,5 @@
 <title>A+ for Teachers</title>
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>css/teacher-course.css">
 <script>setPageName("courses")</script>
 <script src="<?php echo $base_url; ?>js/teacher/nav-menu.js"></script>
 </head>
@@ -6,5 +7,22 @@
 <body>
     <div id="nav-menu-hook"></div>
     <main>
-    <h1>Página de cadeiras para professores</h1>
+        
+        <h1>Página de cadeiras para professores *suposto aceder pelas cadeiras que leciona*</h1>
+        <div class="form-container">
+            <h2>Sumário da Cadeira</h2>
+            <p>*buscar descrição da cadeira à base de dados*</p>
+
+            <br><br>
+
+            <a class="button">Fórum</a>
+            <a href="<?php echo $base_url; ?>app/teacher/projects" class="button">Criar Projeto</a>
+            <a class="button">Projeto 1 *buscar se existir algum projeto*</a>
+            <a class="button">Lista de Alunos</a>
+
+            <br><br>
+
+            <h2>Horário de Dúvidas</h2>
+            <p>*ir buscar info*</p>
+        </div>
     </main>

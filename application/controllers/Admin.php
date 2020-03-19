129b7a1b9190912ddf10b05718f9c820fd68e88d
Eduardo Pereira <fc51630@alunos.fc.ul.pt>
2020-03-19 18:50:03 +0000

    Edu - Import users from csv

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -15,6 +15,7 @@ class Admin extends REST_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->helper(['jwt', 'authorization']);
+        
     }
 
     //admin/api/função
@@ -24,8 +25,9 @@ class Admin extends REST_Controller {
             case "registerCollege": $this->registerCollege(); break; //     admin/api/registerCollege
             case "editUser":        $this->editUser(); break; //            admin/api/editUser
             case "getAdminHome":    $this->getAdminHome(); break; //        admin/api/getAdminHome
+            case "importCSV":       $this ->importCSV(); break; //        admin/api/importCSV
             
-            default:             $this->response("Invalid API call.", parent::HTTP_NOT_FOUND);
+            default:                $this->response("Invalid API call.", parent::HTTP_NOT_FOUND);
         }
     }
 
@@ -35,8 +37,9 @@ class Admin extends REST_Controller {
             case "getAllStudents":  $this->getAllStudents(); break; //      admin/api/getAllStudents
             case "getAllTeachers":  $this->getAllTeachers(); break; //      admin/api/getAllTeachers
             case "saveCSV":         $this->export(); break;
+
             
-            default: $this->response("Invalid API call.", parent::HTTP_NOT_FOUND);
+            default:                $this->response("Invalid API call.", parent::HTTP_NOT_FOUND);
         }
     }
 
@@ -134,6 +137,35 @@ class Admin extends REST_Controller {
         $this -> response($data, parent::HTTP_OK);
     }
 
+    public function importCSV(){
+
+        $this->load->helper('url');
+
+        echo "lol";
+        $this -> load -> model('UserModel');
+        
+        $count_files = $_FILES["userfile"]['tmp_name'];
+        $file  = fopen($count_files, 'r');
+
+        print_r($count_files);
+
+        // Skip first line
+        fgetcsv($file, 0, ","); 
+        while (($column = fgetcsv($file, 0, ",")) !== FALSE) {
+
+            $data = Array(
+                "name"      => $column[0],
+                "surname"   => $column[1],
+                "email"     => $column[2],
+                "role"      => $column[3],
+                "password"  => $column[4]
+            );
+            $this -> UserModel -> registerUser($data);        
+        }
+        header("Location: ". base_url()."app/admin/");
+
+    }
+
     public function export(){
         $this->load->model('UserModel');
        
@@ -145,12 +177,12 @@ class Admin extends REST_Controller {
         $studentsInfo = $this -> UserModel -> getStudents();
 
         $file = fopen('php://output','w');
-        $header = array("Name", "Surname", "Email");
+        $header = array("Name", "Surname", "Email","Role", "Password");
 
         fputcsv($file, $header);
     
         foreach($studentsInfo as $student){
-            $dados = array($student['name'], $student['surname'],$student['email']);
+            $dados = array($student['name'], $student['surname'],$student['email'],$student['role'],$student['password']);
             fputcsv($file, $dados);
         }
         fclose($file);
--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -2,13 +2,13 @@
 <script>setPageName("home")</script>
 <script src="<?php echo $base_url; ?>js/admin/nav-menu.js"></script>
 <script src="<?php echo $base_url; ?>js/admin/home.js"></script>
+
 <link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>css/admin/home.css">
 </head>
 
 <body>
     <div id="nav-menu-hook"></div>
     <main>
-        <h1>Painel de Controlo</h1>
         <section class="admin-stats">
             <div class="admin-stats-window" id="users-hook">
                 <div class="admin-stats-title">Users</div>
@@ -19,7 +19,7 @@
                 </div>
                 <div class="admin-stats-btn">
                     
-                    <a href="<?php echo $base_url; ?>app/admin/users"><div>Manage</div></a>
+                    <a href="<?php echo $base_url; ?>app/admin/registerUser"><div>Manage</div></a>
                 </div>
             </div>
 
@@ -33,4 +33,40 @@
                 </div>
             </div>
         </section>
+
+
+        <div class="form-container">
+            
+        
+            <h1>Import/Export Data</h1>
+
+            
+            <div id="importSide">
+
+                <h3>TODO:</h3>
+                <ul>
+                    <li>Importar utilizadores</li>
+                    <li>Exportar utilizadores</li>
+                </ul>
+
+                <form id="exportCsv" action="<?php echo base_url(); ?>admin/api/saveCSV">
+                    <input type="submit" id="exportInfo" value="Exportar Users">
+                </form>
+            
+            </div>
+
+            <div id="exportSide" >
+                <!-- <form id="file-form" action="javascript:void(0)"> -->
+                <form id="file-form" method="post" action="<?php echo base_url(); ?>admin/api/importCSV" enctype="multipart/form-data">
+                    <!-- <label for="myfile">Importar dados:</label> -->
+                    <input type="file" id="myfile" name="userfile" ><br><br>
+                    <input type="submit" id="import-data-submit"  value="Importar Data">
+                </form>
+            </div>
+
+            
+            <br>
+        
+        </div>
+        
     </main>

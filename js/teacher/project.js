5c888cffc404a288a35d339c2c076a090a974ab4
David Silva <davidmssilva5@gmail.com>
2020-03-25 12:50:36 +0000

    bug tabela etapa resolvido - eliminar ta suave @jxtzzz

--- js/teacher/project.js
+++ js/teacher/project.js
@@ -32,9 +32,9 @@ $(document).ready(() => {
 
     // refresh de segundo em segundo? defini 4 para nao ser demasiado for now
 
-    // setInterval(function(){
-    //      getEtapas(proj);
-    // }, 3000);
+    setInterval(function(){
+         getEtapas(proj);
+    }, 3000);
 
 
     //confirmed delete 
@@ -111,7 +111,7 @@ function makeEtapaTable(data){
         '</table>'
 
 
-    $("#etapas-container").append(table);    
+    $("#etapas-container").html(table);    
 }
 
 function getEtapas(proj_id){

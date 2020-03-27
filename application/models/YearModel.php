b90f294fd75880b3467858854695792deb38b576
Raul Koch <fc51641@alunos.fc.ul.pt>
2020-03-27 17:50:49 +0000

    #raul faltava uma chaveta

--- application/models/YearModel.php
+++ application/models/YearModel.php
@@ -9,6 +9,7 @@ class YearModel extends CI_Model { //ano_letivo
     }
     public function deleteSchoolYear($inicio){
         $this->db->delete('ano_letivo', array('inicio'=>$inicio));
+    }
 
     public function countAcademicYear(){
         return $this->db->count_all_results('ano_letivo');

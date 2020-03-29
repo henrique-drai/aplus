b77c88fe310d9999c8fa6a430001f6db3ead82df
Henrique Francisco <fc51638@alunos.fc.ul.pt>
2020-03-29 20:11:09 +0100

    criei a página de perfil para outros users

--- application/views/app/profile_show.php
+++ application/views/app/profile_show.php
@@ -3,13 +3,34 @@
 <script src="<?php echo $base_url; ?>js/<?php echo $this->session->role; ?>/nav-menu.js"></script>
 <script src="<?php echo $base_url; ?>js/profile.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>css/profile.css">
-</head>
+</head> 
+
+<?php
+// VERIFICAR SE O UTILIZADOR TEM FOTO
+    $picture = "http://files.luzamag.com/profile/". $user->id . ".jpg?" . time();
+    $file_headers = @get_headers($picture);
+    if(!$file_headers || strpos($file_headers[0], '404') !== false) {
+        $picture = $base_url."uploads/profile/default.jpg";
+    }
+?>
 
 <body>
     <div id="nav-menu-hook"></div>
 
     <main>
-        <h1>Perfil do utilizador com email <?php echo $user->email; ?></h1>
-        Listar info sobre este utilizador, sem deixar editar.
-
+        <div class="header">
+            <img src="<?php echo $picture; ?>" alt="Profile Picture">
+            <div class="header-info">
+                <div class="name">
+                    <?php echo $user->name." ". $user->surname; ?>
+                </div>
+                <hr>
+                <div class="bio">
+                    <?php echo $user->description; ?>
+                </div>
+                <div class="email">
+                <?php echo $user->email; ?>
+                </div>
+            </div>
+        </div>
     </main>

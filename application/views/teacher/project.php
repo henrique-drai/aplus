cf67c4a34fe08b79a021fd8f9893a337861dd0a6
David Silva <davidmssilva5@gmail.com>
2020-04-15 17:25:57 +0100

    pequena adição file accept .pdf para evitar erros

--- application/views/teacher/project.php
+++ application/views/teacher/project.php
@@ -20,7 +20,7 @@
         <h3 id="enunciado_h3"></h3>
         <div class="wrapper-top">
             <?php echo form_open_multipart('UploadsC/uploadEnunciadoProjeto', "id='form-upload-proj'");?>
-                <input class="form-input-file" type="file" id="file_projeto" name="file_proj" title="Escolher enunciado">
+                <input class="form-input-file" type="file" id="file_projeto" name="file_proj" title="Escolher enunciado" accept=".pdf">
                 <input id="addEnunciado" type="submit" value="Adicionar enunciado">
             </form>
         </div>
@@ -70,7 +70,7 @@
 
         <?php echo form_open_multipart('UploadsC/uploadEnunciadoEtapa', 'id="form-upload-etapa"');?>
                 <label id="letapa" for="file">Enunciado:</label>
-                <input class="form-input-file" type="file" id="file_etapa" name="file_etapa">
+                <input class="form-input-file" type="file" id="file_etapa" name="file_etapa" accept=".pdf">
                 <input id="addEnuncEtapa" type="submit" value="Adicionar enunciado">
         </form>
 

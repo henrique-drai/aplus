102aff320ea6d1a622c8577c8525ec9265316570
Henrique <henrique.aztn@gmail.com>
2020-04-30 18:53:17 +0100

    restruturação do small_script

--- application/models/ScriptModel.php
+++ application/models/ScriptModel.php
@@ -1,6 +1,10 @@
 <?php
 class ScriptModel extends CI_Model {
 
+  public function batch($table, $array) {
+    $this->db->insert_batch($table, $array);
+  }
+
   public function student($name, $surname, $email, $password, $description) {
     $this->db->delete("user", ['email' => $email]);
     $this->db->insert("user", Array("name"=>$name, "surname"=>$surname, "email"=>$email, "role"=>"student", "password"=>md5($password), "description"=>$description));
@@ -43,6 +47,46 @@ class ScriptModel extends CI_Model {
       $this->db->insert("grupo", Array("projeto_id"=> $projeto_id, "name"=>$name));
       return $this->db->insert_id();
   }
+
+  public function faculdade($name, $siglas, $location) {
+    $this->db->delete("faculdade", ['name' => $name]);
+    $this->db->insert("faculdade", Array("name"=> $name, "siglas"=>$siglas, "location"=>$location));
+    return $this->db->insert_id();
+  }
+
+  public function ano_letivo($inicio, $fim) {
+    $this->db->delete("ano_letivo", ['inicio' => $inicio]);
+    $this->db->insert("ano_letivo", Array("inicio"=> $inicio, "fim"=>$fim));
+    return $this->db->insert_id();
+  }
+
+  public function curso($faculdade_id, $ano_id, $code, $name, $description) {
+    $this->db->insert("curso", Array("faculdade_id"=> $faculdade_id, "ano_letivo_id"=>$ano_id, "code"=>$code, "name"=>$name, "description"=>$description));
+    return $this->db->insert_id();
+  }
+
+  public function aula($cadeira_id, $type, $start_time, $end_time) {
+    $this->db->insert("aula", Array("cadeira_id"=> $cadeira_id, "type"=>$type, "start_time"=>$start_time, "end_time"=>$end_time));
+    return $this->db->insert_id();
+  }
+
+  public function forum($cadeira_id, $name, $description, $teachers_only) {
+    $this->db->insert("forum", Array("cadeira_id"=> $cadeira_id, "name"=>$name, "description"=>$description, "teachers_only"=>$teachers_only));
+    return $this->db->insert_id();
+  }
+
+  public function thread($user_id, $forum_id, $title, $content, $date) {
+    $this->db->insert("thread", Array("user_id"=> $user_id, "forum_id"=>$forum_id, "title"=>$title, "content"=>$content, "date"=>$date));
+    return $this->db->insert_id();
+  }
+
+  public function thread_post($thread_id, $user_id, $content, $date) {
+    $this->db->insert("thread_post", Array("thread_id"=>$thread_id, "user_id"=>$user_id, "content"=>$content, "date"=>$date));
+    return $this->db->insert_id();
+  }  
+
+  public function etapa_submit($grupo_id, $etapa_id, $submit_url) {
+    $this->db->insert("etapa_submit", Array("grupo_id"=>$grupo_id, "etapa_id"=>$etapa_id, "submit_url"=>$submit_url));
+    return $this->db->insert_id();
+  }  
 }
-
-

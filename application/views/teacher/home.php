2cc3eb44cb9ab448ea9fe9515a61321f0489b387
Ines Sousa <fc51588@alunos.fc.ul.pt>
2020-05-09 19:28:47 +0100

    @iness novo painel de controlo gostoso

--- application/views/teacher/home.php
+++ application/views/teacher/home.php
@@ -1,37 +1,20 @@
-<title>A+ for Teachers</title>
-<link rel="stylesheet" type="text/css" href="<?=$base_url?>css/teacher/home.css">
+<title>A+ for Students</title>
 <script>setPageName("home")</script>
-<script src="<?=$base_url?>js/teacher/home.js"></script>
 <link rel="stylesheet" type="text/css" href="<?=$base_url?>css/calendario.css">
+<link rel="stylesheet" type="text/css" href="<?=$base_url?>css/home.css">
+<script>setRole("teacher")</script>
 <script src="<?=$base_url?>js/calendario.js"></script>
+<script src="<?=$base_url?>js/home.js"></script>
 </head>
 
 <body>
 <?php $this->view('templates/nav-menu'); ?>
 <?php $this->view('templates/popup'); ?>
-    <main>
+<main>
     <h1>Painel de Controlo</h1>
-
     <h3 style="text-align: center">Calendário</h3>
     <div id="calendario-hook"></div>
-    <br><br>
 
-    <section class="prof-stats">
-        <div class="prof-stats-window" id="cadeiras-hook">
-            <div class="prof-stats-title">Cadeiras</div>
-            <br>
-            <div class="prof-stats-content">
-                Nº de Cadeiras: <span id="hook-num-cadeiras"></span>
-            </div>
-            <div class="prof-subjects"></div>
-        </div>
-
-        <div class="prof-stats-window" id="alunos-hook">
-            <div class="prof-stats-title">Alunos</div>
-            <br>
-            <div class="prof-stats-content">
-                Nº de Alunos: <span id="hook-num-alunos"></span>
-            </div>
-        </div>
-    </section>
-    </main>
+    <h3 style="text-align: center">Cadeiras acedidas recentemente</h3>
+    <div id="subjects-hook"></div>
+</main>
---
+++ js/home.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+    loadHome();
+    // setInterval(loadProfHome, 3000); 
+
+    $("body").on("click", "a", function() {
+        var link = $(this).attr("href");
+        localStorage.setItem("year", link.split("/").pop());
+    })
+})
+
+function loadHome() {
+    $.ajax({
+        type: "GET",
+        headers: {
+            "Authorization": localStorage.token
+        },
+        url: base_url + "api/getCadeirasOrder/" + localStorage.user_id + "/" + user_role,
+        success: function(data) {
+            console.log(data)
+            $("#subjects-hook").empty();
+
+            if(data.cadeiras_id.length != 0) {
+                var image_url = base_url + "uploads/pattern.jpg";
+                $("#subjects-hook").append("<div class='cadeiras'></div>");
+
+                for(var i = 0; i < 3; i++) {
+                    var url = base_url + "subjects/subject/" + data.info[i][0].code + "/" + data.year[0].inicio;
+                    var color = convertHex(data.info[i][0].color, 52);
+
+                    $(".cadeiras").append("<a id='" + data.info[i][0].code + "' href='" + url +
+                    "'><div class='card_info'><div class='color' style='background-image: linear-gradient(to bottom, " + color + ", " + color + ")," +
+                    "url(" + image_url + "); height: 69%;'></div><div class='subject'>" +
+                    "<div id='title'>Tecnologias de Informação</div><div>" + data.info[i][0].code + ": " +
+                    data.info[i][0].name + "</div></div></div></a>");                
+                }
+
+            }
+            
+        },
+        error: function(data) {
+            alert("Houve um erro ao ir buscar a informação das cadeiras lecionadas.");
+        }
+    })
+}
+
+function convertHex(hex,opacity){
+
+    hex = hex.replace('#','');
+    r = parseInt(hex.substring(0,2), 16);
+    g = parseInt(hex.substring(2,4), 16);
+    b = parseInt(hex.substring(4,6), 16);
+
+    result = 'rgba('+r+','+g+','+b+','+opacity/100+')';
+    return result;
+
+}

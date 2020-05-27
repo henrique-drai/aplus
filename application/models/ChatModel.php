76b2f99aa64f51af4ec7e829a424badb417e9296
Raul Koch <fc51641@alunos.fc.ul.pt>
2020-05-27 06:32:46 +0100

    #raul omfg nunca funciona a primeira ate daqui a 5 horas enter not working

--- application/models/ChatModel.php
+++ application/models/ChatModel.php
@@ -13,13 +13,13 @@ class ChatModel extends CI_Model { //provate_chat & private_chat_msg
 
     
     public function getChatLogs($session_id){ 
-        $query = $this->db->query("SELECT id_sender,COUNT(id_sender) AS num_msg,content,MAX(date) AS lastReceived FROM private_msg WHERE id_receiver=$session_id OR id_sender=$session_id GROUP BY id_sender ORDER BY date DESC");
+        $query = $this->db->query("SELECT * FROM private_msg WHERE (id_sender, date) IN (SELECT id_sender, MAX(date) FROM private_msg WHERE id_receiver=$session_id GROUP BY id_sender)");
         $result = $query->result_array();
         return $result;
     }
 
     public function getChatHistory($session_id, $id_sender){
-        $query = $this->db->query("SELECT * FROM private_msg WHERE (id_receiver=$session_id AND id_sender=$id_sender) OR (id_receiver=$id_sender AND id_sender=$session_id) ORDER BY date");
+        $query = $this->db->query("SELECT * FROM private_msg WHERE (id_receiver=$session_id AND id_sender=$id_sender) OR (id_receiver=$id_sender AND id_sender=$session_id) ORDER BY date ASC");
         $result = $query->result_array();
         return $result;
     }

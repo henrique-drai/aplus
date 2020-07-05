cead3319c2e7ea9ee7aee51c31fbbcbd92033a05
Henrique <fc51638@alunos.fc.ul.pt>
2020-07-06 00:46:41 +0100

    abri 443

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,5 @@ RUN a2enmod rewrite
 COPY ./ /var/www/html/
 RUN service apache2 restart
 #RUN /bin/composer install --no-dev --prefer-source
-EXPOSE 80 
-
-
+EXPOSE 80
+EXPOSE 443
--- application/hooks/ssl.php
+++ application/hooks/ssl.php
@@ -3,11 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 function redirect_ssl() {
 
-    $CI =& get_instance();
+    // $CI =& get_instance();
 
-    if(strpos ( $_SERVER['SERVER_NAME'] , 'elasticbeanstalk' ) ) {
+    // if(strpos ( $_SERVER['SERVER_NAME'] , 'elasticbeanstalk' ) ) {
 
-        header('Location: ' . $CI->config->config['base_url']);
-        exit;
-    }
+    //     header('Location: ' . $CI->config->config['base_url']);
+    //     exit;
+    // }
 }

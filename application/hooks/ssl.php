765056b98bfe2abcb0c6b7085288a53809616a0c
Ines Sousa <fc51588@alunos.fc.ul.pt>
2020-07-11 20:27:35 +0100

    @iness new security headers

--- application/hooks/ssl.php
+++ application/hooks/ssl.php
@@ -1,6 +1,17 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+header("Cache-Control: no-cache, must-revalidate");
+header('Content-Type: text/html; charset=utf-8');
+header("X-Clacks-Overhead: GNU Terry Pratchett");
+header("Content-Language: en");
+header("X-XSS-Protection: 1; mode=block");
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: no-referrer-when-downgrade");
+header("Feature-Policy: sync-xhr *");
+header("Content-Security-Policy: img-src: script-src");
+
 function redirect_ssl() {
 
     // $CI =& get_instance();

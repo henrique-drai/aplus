e4dfd34c5d6978868e8b14b5091676e9d7aa81f8
Ines Sousa <fc51588@alunos.fc.ul.pt>
2020-07-12 11:57:52 +0100

    @iness asd

--- application/hooks/ssl.php
+++ application/hooks/ssl.php
@@ -10,7 +10,7 @@ header("X-Frame-Options: SAMEORIGIN");
 header("X-Content-Type-Options: nosniff");
 header("Referrer-Policy: no-referrer-when-downgrade");
 header("Feature-Policy: sync-xhr *");
-header("Content-Security-Policy: img-src: script-src");
+header("Content-Security-Policy: img-src; script-src");
 
 function redirect_ssl() {
 
